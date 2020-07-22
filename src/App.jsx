6af1e4f2896e7fc42e80fbbd0661cf9cbaedaf72
happytalk-gi <wkdrjsdlf2@gamil.com>
2020-07-22 18:40:38 +0900

    t

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,12 @@ import React from 'react';
 import InputContainer from "./InputContainer";
 import ListContainer from "./ListContainer";
 
-
 export default function App() {
-
     return (
         <div>
-            <h1>To-Do</h1>
+            <h1>TODO</h1>
             <InputContainer/>
             <ListContainer/>
         </div>
-
     );
 }
