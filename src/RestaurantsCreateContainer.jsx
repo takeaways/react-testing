a14d8884a00c1e662da2753d0eeeacf73a7b31c6
JangGeonil <wkdrjsdlf2@gmail.com>
2020-07-28 00:05:14 +0900

    restaurants create container

---
+++ src/RestaurantsCreateContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function RestaurantsCreateContainer() {
+  return (
+    <form>
+      <p>
+        <input type='text' placeholder='레스토랑을 입력해주세요.' />
+        <button type='submit'>등록</button>
+      </p>
+    </form>
+  );
+}
